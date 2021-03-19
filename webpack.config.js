1e4d5645b1bc95812a267cca0261772c3a269f32
Rafal <rporowski@interia.pl>
2021-03-19 10:12:40 +0100

    Completed desktop and mobile styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,29 @@
-const path = require('path')
+const path = require("path")
 
-const postCSSPlugins = [
-  require('postcss-import'),
-  require('postcss-mixins'),
-  require('postcss-simple-vars'),
-  require('postcss-nested'),
-  require('postcss-hexrgba'),
-  require('autoprefixer')
-]
+const postCSSPlugins = [require("postcss-import"), require("postcss-mixins"), require("postcss-simple-vars"), require("postcss-nested"), require("postcss-hexrgba"), require("autoprefixer")]
 
 module.exports = {
-  entry: './app/assets/scripts/App.js',
+  entry: "./app/assets/scripts/App.js",
   output: {
-    filename: 'bundled.js',
-    path: path.resolve(__dirname, 'app')
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"),
   },
   devServer: {
-    before: function(app, server) {
-      server._watch('./app/**/*.html')
+    before: function (app, server) {
+      server._watch("./app/**/*.html")
     },
-    contentBase: path.join(__dirname, 'app'),
+    contentBase: path.join(__dirname, "app"),
     hot: true,
     port: 3000,
-    host: '0.0.0.0'
+    host: "0.0.0.0",
   },
-  mode: 'development',
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader?url=false", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
-      }
-    ]
-  }
+        use: ["style-loader", "css-loader?url=false", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }],
+      },
+    ],
+  },
 }
